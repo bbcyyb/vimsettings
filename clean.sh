fe55904e1dd48119e76e0a110326c4dedb809319
Yabing Yu <bbcyyb@gmail.com>
2017-07-28 21:23:08 +0800

    Add clean.sh for cleaning all vim file at HOME folder.

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo '=======> Clean all vim info at HOME folder.'
+echo '1. rm -rf ~/.vimrc'
+rm -rf ~/.vimrc
+echo '2. rm -rf ~/.vim/'
+rm -rf ~/.vim/
+echo 'Clean finished. <======='
