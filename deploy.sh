5e49859187d7d60fde03ced6980c4584f0f70ae8
Kevin.Y.Yu <bbcyyb@gmail.com>
2017-08-24 11:34:59 +0800

    Add new tmux conf properties.

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ echo '1. cp -av ./.vimrc ~/.vimrc'
 cp -av ./.vimrc ~/.vimrc
 echo '2. cp -av ./.vim/* ~/.vim/'
 cp -av ./.vim/* ~/.vim/
+echo '3. cp -av ./.tmux.conf ~/.tmux.conf'
+cp -av ./.tmux.conf ~/.tmux.conf
 echo 'Deploy finished. <======='
 
